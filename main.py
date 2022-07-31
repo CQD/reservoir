3277317acc7b772eb3e7a3bcf567f804b19502a9
CQD <cqd@hiigara.net>
2022-07-31 15:27:00 +0800

    匯入歷史資料
    以天下文化的資料為準，似乎包含 40 萬立方公尺以上的水庫。

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ def index():
     return FileResponse('public/index.html')
 
 
-@app.get("/api")
-def api():
-    return {"Hello": "World"}
+@app.get("/api/reservoir-history.tsv")
+def reservoir_history():
+    return FileResponse('public/reservoir-history.tsv')
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=80, host='0.0.0.0', reload=True)
