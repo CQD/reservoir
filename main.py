14027da98cb2eab19a688b15d145e354e6d0463d
CQD <cqd@hiigara.net>
2023-04-20 23:31:30 +0800

    資料改為每小時更新

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from concurrent.futures import ThreadPoolExecutor
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 import logging
 from pathlib import Path
+from threading import Timer
 from zoneinfo import ZoneInfo
 
 from fastapi import FastAPI
@@ -13,11 +13,40 @@ from app.data import ReservoirCrawler
 logger = logging.getLogger(__name__)
 app = FastAPI()
 
-thread_pool = ThreadPoolExecutor(max_workers=1)
 
 TSV_CONTENT = Path('public/reservoir-history.tsv').read_text()
 TSV_SUPPLEMENTAL = ''
-TSV_YESTERDAY = ''
+TSV_LATEST = ''
+
+UPDATE_TIME = last_date_str = TSV_CONTENT[-11:-1]
+UPDATE_INTERVAL = 3600
+UPDATE_TIMER: Timer = None
+
+
+@app.on_event("startup")
+def startup():
+    global UPDATE_TIMER
+
+    Timer(1, fetch_new_data).start()
+    logger.warning("[startup] 排定撈最新資料")
+
+    def updater():
+        logger.warning("[updater] 啟動")
+        fetch_new_data()
+
+        UPDATE_TIMER = Timer(UPDATE_INTERVAL, updater)
+        UPDATE_TIMER.start()
+        logger.warning("[updater] 結束")
+
+    UPDATE_TIMER = Timer(UPDATE_INTERVAL, updater)
+    UPDATE_TIMER.start()
+    logger.warning("[startup] 排定更新資料")
+
+
+@app.on_event("shutdown")
+def shutdown():
+    UPDATE_TIMER.cancel()
+    logger.warning("[shutdown] 已關閉 updater")
 
 
 @app.get("/")
@@ -45,26 +74,25 @@ async def robots():
     return FileResponse('public/robots.txt')
 
 
+@app.get("/api/update-time")
+async def update_time():
+    cache_time = UPDATE_INTERVAL if TSV_LATEST else 30
+    headers = {'Cache-Control': f'public, max-age={cache_time}'}
+    return PlainTextResponse(UPDATE_TIME, headers=headers)
+
+
 @app.get("/api/reservoir-history.tsv")
 async def reservoir_history():
-    result_tsv = TSV_CONTENT + TSV_SUPPLEMENTAL + TSV_YESTERDAY
-
-    yesterday = datetime.now(ZoneInfo("Asia/Taipei")).date() - timedelta(days=1)
-    yesterday_str = yesterday.strftime('%Y-%m-%d')
-
-    last_date_str = result_tsv[-11:-1]
-
-    cache_time = 86400
-    if last_date_str < yesterday_str:
-        thread_pool.submit(fetch_new_data)
-        cache_time = 600
-
+    result_tsv = TSV_CONTENT + TSV_SUPPLEMENTAL + TSV_LATEST
+    cache_time = UPDATE_INTERVAL if TSV_LATEST else 30
     headers = {'Cache-Control': f'public, max-age={cache_time}'}
     return PlainTextResponse(result_tsv, headers=headers)
 
 
 def fetch_new_data():
-    global TSV_SUPPLEMENTAL, TSV_YESTERDAY
+    global TSV_SUPPLEMENTAL, TSV_LATEST, UPDATE_TIME
+
+    logger.info("[fetch_new_data] 開始")
 
     # 更新固定資料點的資料
     tsv = TSV_SUPPLEMENTAL if TSV_SUPPLEMENTAL else TSV_CONTENT
@@ -73,26 +101,22 @@ def fetch_new_data():
     yy, mm, dd = map(lambda val_str: int(val_str), last_date_str.split("-"))
     last_date = date(yy, mm, dd)
 
-    logger.warn(f"最新資料時間是 {last_date}，撈取更新的資料")
+    logger.warning(f"最新資料時間是 {last_date}，撈取更新的資料")
 
     crawer = ReservoirCrawler()
     TSV_SUPPLEMENTAL += crawer.fetch_uppdated_as_tsv(last_date=last_date)
+    logger.warning("[fetch_new_data] 固定資料點已更新")
 
-    # 更新「昨天」的資料
-    yesterday = datetime.now(ZoneInfo("Asia/Taipei")).date() - timedelta(days=1)
-    yesterday_str = yesterday.strftime('%Y-%m-%d')
+    # 拉最新的資料
+    now = datetime.now(ZoneInfo("Asia/Taipei"))
+    today_str = now.strftime('%Y-%m-%d')
 
-    last_date_str = '2022-01-01'
-    if TSV_YESTERDAY:
-        last_date_str = TSV_YESTERDAY[-11:-1]
+    lines = [f"{name}\t{max}\t{curr}\t{today_str}\n"
+                for name, (max, curr) in crawer.fetch().items()]
+    TSV_LATEST = "".join(lines)
+    logger.warning("[fetch_new_data] 最新資料點已更新")
 
-    if last_date_str < yesterday_str:
-        lines = [f"{name}\t{max}\t{curr}\t{yesterday_str}\n"
-                 for name, (max, curr) in crawer.fetch(yesterday).items()]
-        TSV_YESTERDAY = "".join(lines)
-
-
-thread_pool.submit(fetch_new_data)
+    UPDATE_TIME = now.strftime('%Y-%m-%d %H:%M:%S')
 
 
 if __name__ == '__main__':
